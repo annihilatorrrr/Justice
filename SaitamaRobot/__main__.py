5e45ec8199be242203713bfc39e8947d02fa3d14
Dʀɪᴄᴋᴡᴏʀʟᴅ <69191476+EmiliaDevs@users.noreply.github.com>
2021-01-27 13:30:52 +0530

    [__main__] reply >> reply_text

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -201,7 +201,6 @@ def start(update: Update, context: CallbackContext):
         else:
             first_name = update.effective_user.first_name
             update.effective_message.reply_text(
-                
                 PM_START_TEXT.format(
                     escape_markdown(first_name), escape_markdown(context.bot.first_name)
                 ),
@@ -236,7 +235,7 @@ def start(update: Update, context: CallbackContext):
                         [
                             InlineKeyboardButton(
                                 text="🗄 Source code",
-                                url="https://github.com/AnimeKaizoku/SaitamaRobot",
+                                url="https://fuckoff.com",
                             )
                         ],
                     ]
